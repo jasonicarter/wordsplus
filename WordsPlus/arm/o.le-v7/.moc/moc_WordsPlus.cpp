02032c41c29407b0ee96e1be53d872c550599458
jason carter <jason.ian.carter@hotmail.com>
2013-01-10 23:45:59 -0500

    Before OS device update - working

--- WordsPlus/arm/o.le-v7/.moc/moc_WordsPlus.cpp
+++ WordsPlus/arm/o.le-v7/.moc/moc_WordsPlus.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'WordsPlus.hpp'
 **
-** Created: Thu Jan 10 14:46:39 2013
+** Created: Thu Jan 10 23:30:17 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.4)
 **
 ** WARNING! All changes made in this file will be lost!
